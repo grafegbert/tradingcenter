64c52af7f84e676435970ea8c819126aafd0dd5c
Lunara500 <lunara4games@gmail.com>
2025-01-14 14:30:11 +0100

    add existing project

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { LoginMaskComponent } from './login-mask/login-mask.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonCardDialogComponent } from './pokemon-card-dialog/pokemon-card-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoadingAnimationComponent } from './loading-animation/loading-animation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    HomepageComponent,
+    FooterComponent,
+    LoginMaskComponent,
+    PokemonCardComponent,
+    PokemonCardDialogComponent,
+    LoadingAnimationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatMenuModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
